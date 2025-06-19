f6b340d47c2e0681622cf0baaca0868a1266931d
kshithij <kshithijy@gmail.com>
2025-06-19 18:18:02 +0530

    feat: add file and directory creation functions to test_tools.py and update execution counts in test1.ipynb

--- test_tools.py
+++ test_tools.py
@@ -1,23 +1,37 @@
-from os import listdir
+import os
 
 all_file=[]
 
+def create_file(name:str,path:str,content:str)->str:
+    try:
+        open(path+"/"+name,"w").write(content)
+    except Exception as e: 
+        return str(e)
+    return "done"
+
+def create_dir(name,path)->str:
+    try:
+        os.mkdir(path+"/"+name)
+    except Exception as e:
+        return str(e)
+    return "done"
+
 def files_tool(path):
     global all_file
-    all_file=[path+"/"+i for i in listdir(path)]
+    all_file=[path+"/"+i for i in os.listdir(path)]
 
     def get_allfiles1(path):
         for i in open(".foldignore","r").read().split("\n"):
             if i in path:
                 return False
         try:
-            listdir(path)
+            os.listdir(path)
         except:
             return False
-        for i in listdir(path):
+        for i in os.listdir(path):
             temp=path+"/"+i
             if get_allfiles1(temp):
-                for j in listdir(temp):
+                for j in os.listdir(temp):
                     all_file.append(temp+"/"+j)
         return True
     get_allfiles1(path)
@@ -28,4 +42,4 @@ def files_tool(path):
 if __name__=="__main__":
     from pprint import pprint
     
-    pprint(files_tool("./my-app"))
+    pprint(create_dir(".venv","."))
